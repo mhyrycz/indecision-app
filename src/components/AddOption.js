798dc587e3ce9e103107fc981bb58ba371f7911a
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2018-12-07 12:43:24 +0100

    6.57 dev-server added to webpack

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -12,7 +12,6 @@ class AddOption extends React.Component {
   }
   handleAddOption(e) {
     e.preventDefault();
-    console.log(shit)
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option)
     this.setState( () => ({ error }) );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-//entry -> output
 const path = require('path')
 
 module.exports = {
@@ -14,7 +13,8 @@ module.exports = {
       exclude: /node_modules/
     }]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
 };
-
-//loader - customize bahavior of webpack
