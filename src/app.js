f638ffb5e567efa843bc044e416df685cb4945f5
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2018-12-07 13:28:09 +0100

    6.58 babel-plugin-transform-class-properties added and basic usage for old/new syntax

--- src/app.js
+++ src/app.js
@@ -3,3 +3,24 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp.js'
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+
+
+class OldSyntax {
+  constructor(props) {
+    this.name = 'Maciek';
+  }
+  getGreeting(){
+    return `My name is ${this.name}`
+  }
+}
+const OldSyntaxInstance = new OldSyntax();
+console.log(OldSyntaxInstance.getGreeting()); // should not work with old syntax and without plugin
+//------------------
+class NewSyntax {
+  name = 'Maciek';
+  getGreeting = () => {
+    return `My name is ${this.name}`
+  }
+}
+const NewSyntaxInstance = new NewSyntax();
+console.log(NewSyntaxInstance.getGreeting());
