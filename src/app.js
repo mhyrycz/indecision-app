1fd2c089551b2d81f36fa2b5ff7e0317af322b94
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2018-12-07 10:52:42 +0100

    imported react, react-dom and validator (npm modules) by yarn add

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-import asIWant, {square, add} from './utils.js';
-import {isAdult, canDrink, canDrink2} from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const template = React.createElement('p', {}, 'testing123')
 
-
-console.log('webpack is running!');
-console.log(square(5));
-console.log(add(5,6));
-console.log(asIWant(5,6));
-console.log(isAdult(18))
-console.log(canDrink(18))
-console.log(canDrink2(18))
+ReactDOM.render(template, document.getElementById('app'))
