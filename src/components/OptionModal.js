90f03cbe003b34af912cd94456d391f1615b046f
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2018-12-09 13:04:05 +0100

    8.71 React-Modal component custom styling added

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,17 @@ const OptionModal = (props) => (
       isOpen={!!props.selectedOption}
       onRequestClose={props.handleClearSelected}
       contentLabel="Selected Option"
+      closeTimeoutMS={200}
+      className='modal'
     >
-      <h3>Selected Option</h3>
-      {props.selectedOption && <p>your option: {props.selectedOption}</p>}
-      <button onClick={props.handleClearSelected}>Okay</button>
+      <h3 className='modal__title'>Selected Option</h3>
+      {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
+      <button
+        className='button'
+        onClick={props.handleClearSelected}
+      >
+        Okey
+      </button>
     </Modal>
   );
 
